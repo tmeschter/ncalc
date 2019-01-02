5fed0cb14b53aa6ee9c79c2d0f20022503410aa5
Tom Meschter <tom.meschter@valinor.org>
2019-01-02 14:40:41 -0800

    Move TransformResult to its own file

--- NCalcLib/Transformer.cs
+++ NCalcLib/Transformer.cs
@@ -5,32 +5,6 @@ using LinqExpression = System.Linq.Expressions.Expression;
 
 namespace NCalcLib
 {
-    public sealed class TransformResult
-    {
-        public TransformResult(BindingContext bindingContext, LinqExpression expression, ImmutableList<string> errors)
-        {
-            BindingContext = bindingContext;
-            Expression = expression;
-            Errors = errors;
-        }
-
-        public TransformResult(BindingContext bindingContext, LinqExpression expression)
-            : this(bindingContext, expression, ImmutableList<string>.Empty)
-        {
-        }
-
-        public BindingContext BindingContext { get; }
-        public LinqExpression Expression { get; }
-        public ImmutableList<string> Errors { get; }
-
-        public void Deconstruct(out BindingContext bindingContext, out LinqExpression expression, out ImmutableList<string> errors)
-        {
-            bindingContext = BindingContext;
-            expression = Expression;
-            errors = Errors;
-        }
-    }
-
     public class Transformer
     {
         public static TransformResult Transform(BindingContext bindingContext, Expression e)
