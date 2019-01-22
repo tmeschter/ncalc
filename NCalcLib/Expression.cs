ddbb12e93f3621f9b0ba9576ae506a24f9b0ead6
Tom Meschter <tom.meschter@valinor.org>
2019-01-21 20:18:45 -0800

    Support start and length
    Update `Expression` to support `Start` and `Length` properties, along with equivalents that include leading whitespace.

--- NCalcLib/Expression.cs
+++ NCalcLib/Expression.cs
@@ -4,6 +4,11 @@ namespace NCalcLib
 {
     public abstract class Expression : IEquatable<Expression>
     {
+        public abstract int Start();
+        public abstract int StartWithWhitespace();
+        public abstract int Length();
+        public abstract int LengthWithWhitespace();
+
         public abstract bool Equals(Expression other);
         public abstract override bool Equals(object obj);
         public abstract override int GetHashCode();
